8ec12d3ebdadf8186859497a4ac02977349460e6
z-tier <fglemeric@gmail.com>
2025-05-07 18:13:54 +0800

    Delete mini jam/arithmageddon directory

--- mini jam/arithmageddon/scripts/globals/points_handler.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-
-var points: int = 0
-var combo_multiplier: int = 1
--- mini jam/arithmageddon/scripts/operations/operation.gd
+++
@@ -1,6 +0,0 @@
-class_name Operation
-extends Resource
-
-
-@export var icon: String
-@export var effect: Script
